885f5d3bb65cb7240d7a152d81d1622b049a407e
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-05-09 19:35:58 -0400

    created cart page

---
+++ src/app/orders/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+// import styles from "@/styles/Order.module.css";
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return (
+    <div className="stylesContainer">
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default page;
